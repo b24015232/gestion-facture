4f276a3a389459fea9a738693ea6f08a08e6129e
ilian-marchitto <ilian.marchitto@etu.univ-amu.fr>
2026-02-28 13:55:06 +0100

    ajout des fichiers de configuration de la BD

--- config/config.php
+++ config/config.php
@@ -1,10 +1,24 @@
 <?php
 
+// On charge les variables du .env
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        // On ignore les commentaires
+        if (strpos(trim($line), '#') === 0) continue;
+
+        // On sépare la clé et la valeur
+        list($name, $value) = explode('=', $line, 2);
+        $_ENV[trim($name)] = trim($value);
+    }
+}
+
 // Variable BD
-define('DB_HOST', 'mysql-toncompte.alwaysdata.net');
-define('DB_NAME', 'toncompte_nom_bd');
-define('DB_USER', 'toncompte_user');
-define('DB_PASS', 'ton_mot_de_passe_secret');
+define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
+define('DB_NAME', $_ENV['DB_NAME'] ?? '');
+define('DB_USER', $_ENV['DB_USER'] ?? '');
+define('DB_PASS', $_ENV['DB_PASS'] ?? '');
 
 // Variable URL
 define('URL_ROOT', 'https://ton-site.alwaysdata.net'); 
