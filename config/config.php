acb89e9288a7995968bf7ce2181088c3b7f4b9e5
ilian-marchitto <ilian.marchitto@etu.univ-amu.fr>
2026-02-28 13:43:09 +0100

    creation modele Config.php

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Variable BD
+define('DB_HOST', 'mysql-toncompte.alwaysdata.net');
+define('DB_NAME', 'toncompte_nom_bd');
+define('DB_USER', 'toncompte_user');
+define('DB_PASS', 'ton_mot_de_passe_secret');
+
+// Variable URL
+define('URL_ROOT', 'https://ton-site.alwaysdata.net'); 
+
+// Fuseau horaire
+date_default_timezone_set('Europe/Paris');
